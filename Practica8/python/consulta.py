8a4120dfb9ef863648773eea08138d3d1a161645
victord62 <126619686+victord62@users.noreply.github.com>
2024-12-01 00:16:02 +0100

    tercer avance practica

--- Practica8/python/consulta.py
+++ Practica8/python/consulta.py
@@ -1,63 +1,112 @@
-# -*- coding: utf-8 -*-
-
-###########################################################################
-## Python code generated with wxFormBuilder (version 4.2.1-0-g80c4cb6)
-## http://www.wxformbuilder.org/
-##
-## PLEASE DO *NOT* EDIT THIS FILE!
-###########################################################################
-
 import wx
 import wx.xrc
-
 import gettext
+import datetime
 import os
 _ = gettext.gettext
 
-###########################################################################
-## Class MyFrame1
-###########################################################################
 
-class MyFrame1 ( wx.Frame ):
+## Ventana para la consulta de la lista de todos los vehiculos del taller
+##
+## Autores: Luis Setién, Victor Descalzo
+## Version: Diciembre 2024
+####
+class VentanaConsulta ( wx.Frame ):
 
+    ## Metodo que se encarga de inicializar el contenido de la ventana
     def __init__( self, parent ):
-        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = "Consultar lista vehículos", pos = wx.DefaultPosition, size = wx.Size( 600,400 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
         self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+        self.SetBackgroundColour( wx.Colour( 255, 255, 255 ) )
 
-        bSizer1 = wx.BoxSizer( wx.VERTICAL )
-        bSizer1.SetMinSize( wx.Size( 500,300 ) )
+        # establecer icono de la ventana
+        imagen = wx.Bitmap("./imagenes/icono.png", wx.BITMAP_TYPE_PNG)
+        icono = wx.Icon()
+        icono.CopyFromBitmap(imagen)
+        self.SetIcon(icono)
 
-        
-        self.m_staticText1 = wx.StaticText( self, wx.ID_ANY, _(u"MyLabel"), wx.DefaultPosition, wx.DefaultSize, 0 )
-        self.m_staticText1.Wrap( -1 )
-        bSizer1.Add( self.m_staticText1, 0, wx.ALL, 5 )
+        # establecer dimensionado de los elementos
+        boxSizer = wx.BoxSizer( wx.VERTICAL )
+        self.SetMinSize( wx.Size( 500,300 ) )
 
-        self.SetSizer( bSizer1 )
+        # agregar la barra de herramientas
+        self.toolBar = wx.ToolBar( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TB_FLAT|wx.TB_HORZ_TEXT )
+        self.toolBar.SetMargins( wx.Size( 50,50 ) )
+        self.toolBar.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_3DLIGHT ) )
+
+        # agregar los elementos a la barra de herramientas junto a las acciones que desencadenan
+        self.toolImprimir = self.toolBar.AddTool( 1, _(u"Imprimir"), wx.Bitmap( u"./imagenes/imprimir.png", wx.BITMAP_TYPE_ANY ), wx.NullBitmap, wx.ITEM_NORMAL, wx.EmptyString, "Imprime en papel la lista completa de vehículos en el taller.", None )
+        self.Bind(wx.EVT_TOOL, self.toolImprimirClicked, id = 1)
+
+        self.toolActualizar = self.toolBar.AddTool( 2, _(u"Actualizar"), wx.Bitmap( u"./imagenes/actualizar.png", wx.BITMAP_TYPE_ANY ), wx.NullBitmap, wx.ITEM_NORMAL, wx.EmptyString, "Actualiza los datos de todos los vehículos a su última revisión.", None )
+        self.Bind(wx.EVT_TOOL, self.toolActualizarClicked, id = 2)
+        self.toolBar.AddSeparator()
+
+        self.toolSalir = self.toolBar.AddTool( 3, _(u"Salir"), wx.Bitmap( u"./imagenes/salir.png", wx.BITMAP_TYPE_ANY ), wx.NullBitmap, wx.ITEM_NORMAL, wx.EmptyString, "Salir de esta ventana.", None )
+        self.Bind(wx.EVT_TOOL, self.toolSalirClicked, id = 3)
+
+        self.toolBar.Realize()
+        boxSizer.Add( self.toolBar, 0, wx.EXPAND, 5 )
+
+        # mostrar la tabla de entradas de los vehiculos
+        #self.m_staticText1 = wx.StaticText( self, wx.ID_ANY, _(u"MyLabel"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        #self.m_staticText1.Wrap( -1 )
+        #boxSizer.Add( self.m_staticText1, 0, wx.ALL, 5 )
+
+        # obtener la fecha y hora actual en el formato adecuado
+        fecha_hora = datetime.datetime.now()
+        fecha_hora_string = fecha_hora.strftime("%d/%m/%Y %H:%M:%S")
+
+        # agregar la barra de estado y mostrar el texto por defecto
+        self.statusBar = self.CreateStatusBar( 1, wx.STB_SIZEGRIP, wx.ID_ANY )
+        self.statusBar.SetStatusText(f"Datos actualizados a {fecha_hora_string}")
+        self.statusBar.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_3DLIGHT ) )
+
+        # ajustar posicionamiento de la ventana
+        self.SetSizer( boxSizer )
         self.Layout()
-
         self.Centre( wx.BOTH )
         self.Show()
 
-    def __del__( self ):
-        pass
+
+    ## Metodo que se encarga de la accion realizada al pulsar el boton de imprimir
+    def toolImprimirClicked(self, event):
+        wx.MessageBox("Sin implementar", " ", wx.OK | wx.ICON_NONE)
 
 
+    ## Metodo que se encarga de la accion realizada al pulsar el boton de actualizar
+    ## se actualizan los valores de la tabla y se refresca la barra de estado
+    def toolActualizarClicked(self, event):
+        fecha_hora = datetime.datetime.now()
+        fecha_hora_string = fecha_hora.strftime("%d/%m/%Y %H:%M:%S")
+        self.statusBar.SetStatusText(f"Datos actualizados a {fecha_hora_string}")
+        wx.MessageBox("Datos actualizados satisfactoriamente", "Mensaje", wx.OK | wx.ICON_INFORMATION)
+
+
+    ## Metodo que se encarga de la accion realizada al pulsar el boton de salir
+    def toolSalirClicked(self, event):
+        self.Close()
+
+
+## Funcion main que se encarga de invocar la ventana
+## señalizamos la apertura y cierre de la misma mediante un fichero
+####
 if __name__ == "__main__":
     try:
-        open('./control/P_CONSULTA', "w").close()
-    except OSError:
-        print("fail")
+        open("./control/P_CONSULTA", "w").close()
+    except Exception as e:
+        print(f"error: {e}")
     else:
-        print("ok")
+        print("VentanaConsulta abierta")
 
     app = wx.App()
-    frame = MyFrame1(None)
+    frame = VentanaConsulta(None)
     app.MainLoop()
     
     try:
-        os.remove('./control/P_CONSULTA')
+        os.remove("./control/P_CONSULTA")
     except Exception as e:
-        print(f"fail {e}")
+        print(f"error: {e}")
     else:
-        print("ok")
+        print("VentanaConsulta cerrada")
--- Practica8/python/principal.py
+++ Practica8/python/principal.py
@@ -21,20 +21,20 @@ class VentanaPrincipal ( wx.Frame ):
         self.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_MENU ) )
 
         # establecer icono de la ventana
-        imagen = wx.Bitmap('./imagenes/icono.png', wx.BITMAP_TYPE_PNG)
+        imagen = wx.Bitmap("./imagenes/icono.png", wx.BITMAP_TYPE_PNG)
         icono = wx.Icon()
         icono.CopyFromBitmap(imagen)
         self.SetIcon(icono)
 
         # establecer dimensionado de los elementos
         boxSizer = wx.BoxSizer( wx.VERTICAL )
-        boxSizer.SetMinSize( wx.Size( 250,200 ) )
+        self.SetMinSize( wx.Size( 250,200 ) )
 
         # agregar los botones, las acciones que desencadenan y el espaciado vertical entre ellos
         self.botRegistrar = wx.Button( self, wx.ID_ANY, _(u"Registrar nuevo vehículo"), wx.DefaultPosition, wx.Size(180, 30), 0 )
         boxSizer.Add((0, 24), 0, 0, 0)
         boxSizer.Add( self.botRegistrar, 0, wx.CENTER, 5 )
-        #self.botRegistrar.Bind(wx.EVT_BUTTON, self.botRegistrarClicked)
+        self.botRegistrar.Bind(wx.EVT_BUTTON, self.botRegistrarClicked)
 
         self.botConsultar = wx.Button( self, wx.ID_ANY, _(u"Consultar lista vehículos"), wx.DefaultPosition, wx.Size(180, 30), 0 )
         boxSizer.Add((0, 6), 0, 0, 0)
@@ -46,32 +46,41 @@ class VentanaPrincipal ( wx.Frame ):
         boxSizer.Add( self.botSalir, 0, wx.CENTER, 5 )
         self.botSalir.Bind(wx.EVT_BUTTON, self.botSalirClicked)
 
+        # ajustar posicionamiento de la ventana
         self.SetSizer( boxSizer )
         self.Layout()
-        self.Centre( wx.BOTH )
+        self.SetPosition((125, 125))
         self.Show()
 
 
-    ## Metodo que se encarga de la accion realizada al pulsar el boton de salir
-    def botSalirClicked(self, event):
+    ## Metodo que se encarga de la accion realizada al pulsar el boton de registrar
+    ## comprobamos que dicha ventana no se encuentre ya abierta
+    def botRegistrarClicked(self, event):
+        if os.path.exists("./control/P_REGISTRO"):
+            wx.MessageBox("Esta ventana ya se encuentra abierta", "Error", wx.OK | wx.ICON_ERROR)
+        else:
+            subprocess.Popen(["python", "./registro.py"])
 
-        # comprobamos que no quede ninguna ventana abierta
-        if os.path.exists('./control/P_CONSULTA'):
-            wx.MessageBox('Cierre todas las ventanas antes de salir del programa', 'Error', wx.OK | wx.ICON_ERROR)
+
+    ## Metodo que se encarga de la accion realizada al pulsar el boton de consultar
+    ## comprobamos que dicha ventana no se encuentre ya abierta
+    def botConsultarClicked(self, event):
+        if os.path.exists("./control/P_CONSULTA"):
+            wx.MessageBox("Esta ventana ya se encuentra abierta", "Error", wx.OK | wx.ICON_ERROR)
+        else:
+            subprocess.Popen(["python", "./consulta.py"])
+
+
+    ## Metodo que se encarga de la accion realizada al pulsar el boton de salir
+    ## comprobamos que no quede ninguna ventana abierta antes de salir
+    def botSalirClicked(self, event):
+        if os.path.exists("./control/P_REGISTRO") or os.path.exists("./control/P_CONSULTA"):
+            wx.MessageBox("Cierre todas las ventanas antes de salir del programa", "Error", wx.OK | wx.ICON_ERROR)
         else:
             self.Close()
 
 
-    def botConsultarClicked(self, event):
-
-        # comprobamos si la ventana ya esta abierta
-        if os.path.exists('./control/P_CONSULTA'):
-            wx.MessageBox('Esta ventana ya se encuentra abierta', 'Error', wx.OK | wx.ICON_ERROR)
-        else:
-            subprocess.Popen(['python', './consulta.py'])
-
-
-## Funcion main que se encargara de invocar la ventana
+## Funcion main que se encarga de invocar la ventana
 ####
 if __name__ == "__main__":
     app = wx.App()
